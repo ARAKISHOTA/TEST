033ef147b495d8d666bf183ef955fc6b770ed40d
PC_User <PC_User@DESKTOP-TF6KLVE>
2025-08-24 19:48:09 +0900

    課題33 提出

---
+++ HelloWorld_ArakiShota/src/Vehicle/Main.java
@@ -0,0 +1,20 @@
+package Vehicle;
+
+class Main {
+	  public static void main(String[] args) {
+		  Person person1 = new Person("鈴木","太郎", 20, 1.7, 60);
+		  person1.print();
+		  
+		  Person person2 = new Person("山田","花子", 22, 1.5, 40);
+		  person2.print();
+		  
+		  Person.printCount();
+		  
+		  Car car = new Car();
+		  Bicycle bicycle = new Bicycle();
+		  
+		  person1.buy(car);
+		  person1.buy(bicycle);
+	  }
+	  
+}
---
+++ HelloWorld_ArakiShota/src/Vehicle/Person.java
@@ -0,0 +1,47 @@
+package Vehicle;
+
+class Person{
+	 // インスタンスフィールドを定義
+	private static int count = 0;
+	private String firstName, lastName;
+	private int age;
+	private double height, weight;
+	
+	 // コンストラクタを定義しインスタンスフィールドに値をセット
+	 Person(String firstName,String lastName,int age,double height,double weight){
+		 this.firstName = firstName;
+		 this.lastName = lastName;
+		 this.age = age;
+		 this.height = height;
+		 this.weight = weight;
+		 Person.count++;
+	 }
+	 
+	 public String fullName(){
+		  return this.firstName + this.lastName;
+	 }
+	 
+	 public void print(){
+		  System.out.println("名前は" + this.fullName() + "です");
+		  System.out.println("年は" + this.age + "です");
+		  System.out.println("BMIは" + Math.floor(this.bmi()) + "です");
+	 }
+	 
+	 public double bmi(){
+		  return this.weight / this.height / this.height;
+	 }
+	 
+	 public static void printCount() {
+		 System.out.println("\n合計" + Person.count + "人です");
+	 }
+	 
+	 public void buy(Car car) {
+		 car.setOwner(this.fullName());
+		 System.out.println(car.getOwner() + "が購入しました");
+	 }
+	 
+	 public void buy(Bicycle bicycle) {
+		 bicycle.setOwner(this.fullName());
+		 System.out.println(bicycle.getOwner() + "が購入しました");
+	 }
+}
